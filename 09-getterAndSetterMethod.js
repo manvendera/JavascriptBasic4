3a5948dc93b929cad82a79d830c1267a8c8c058f
manvendera pathak <manvenderapathak165@gmail.com>
2023-10-19 15:32:16 +0530

    javasciptbasic4

---
+++ 09-getterAndSetterMethod.js
@@ -0,0 +1,22 @@
+let person = {
+    firstName : "manvendera",
+    lastName : "Pathak",
+
+    // Getter
+    get fullName()
+    {
+        return person.firstName + " " + person.lastName;
+    },
+
+    // Setter
+    set fullName(value)
+    {
+        let name = value.split(" ");
+        this.firstName = name[0];
+        this.lastName = name[1];
+    }
+}
+
+console.log(person.fullName);
+person.fullName = "pathakmanvendera";
+console.log(person.fullName);
---
+++ 10-tryCatch.js
@@ -0,0 +1,34 @@
+let person = {
+    firstName : "manvendera",
+    lastName : "Pathak",
+
+    // Getter
+    get fullName()
+    {
+        return person.firstName + " " + person.lastName;
+    },
+
+    // Setter
+    set fullName(value)
+    {
+        if(typeof value !== 'string')
+        {
+            throw "You must pass a string";
+        }
+        let name = value.split(" ");
+        this.firstName = name[0];
+        this.lastName = name[1];
+    }
+}
+
+console.log(person.fullName);
+person.fullName = "Pathak manvendera";
+console.log(person.fullName);
+
+try{
+    person.fullName = true;
+}
+catch(e){
+    // console.log(e);
+    alert(e);
+}
